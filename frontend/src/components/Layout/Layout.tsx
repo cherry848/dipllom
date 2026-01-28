300f9ed289dfac6f624618cbe48ae05e860a2c81
Cherry <maximkaleushew@gmail.com>
2026-01-28 19:56:54 +0300

    Layot

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router";
 import s from "./layout.module.css";
 import { Header } from "./components/Header/Header";
-import { Modal } from "../Modal/Modal";
 const Layout = () => {
   return (
     <div className={s.container}>
