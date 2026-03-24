18d3debce82abe6344e03b5c2f2ef3c231786759
Bogdanchik5000 <birkunsasha2@gmail.com>
2026-03-24 19:52:43 +0300

    remove unused

--- backend/services/course.service.ts
+++ backend/services/course.service.ts
@@ -1,6 +1,4 @@
 import courseModel from "../models/course.model";
-import reviewModel from "../models/review.model";
-import { Course } from "../types/course.types";
 import { BaseSort } from "../types/types";
 import { QueryOptions } from "mongoose";
 
@@ -21,64 +19,6 @@ class CourseService {
     const courses = await courseModel.find({}, null, query);
 
     return courses;
-  }
-
-  async getPopular(): Promise<Course[]> {
-    const popularCourses = await reviewModel.aggregate([
-      {
-        $group: {
-          _id: "$courseId",
-          avgRating: { $avg: "$rating" },
-          reviewCount: { $sum: 1 },
-        },
-      },
-      {
-        $lookup: {
-          from: "courses",
-          localField: "_id",
-          foreignField: "_id",
-          as: "courseInfo",
-        },
-      },
-      {
-        $unwind: "$courseInfo",
-      },
-      {
-        $addFields: {
-          // Формула взвешенного рейтинга (Bayesian average)
-          weightedRating: {
-            $divide: [
-              {
-                $add: [
-                  { $multiply: ["$avgRating", "$reviewCount"] },
-                  3.5 * 10, // средний рейтинг * минимальное количество отзывов
-                ],
-              },
-              { $add: ["$reviewCount", 10] },
-            ],
-          },
-        },
-      },
-      {
-        $sort: { weightedRating: -1, reviewCount: -1 },
-      },
-      {
-        $limit: 10,
-      },
-      {
-        $project: {
-          _id: "$courseInfo._id",
-          name: "$courseInfo.name",
-          img: "$courseInfo.img",
-          desc: "$courseInfo.desc",
-          avgRating: 1,
-          reviewCount: 1,
-          weightedRating: 1,
-        },
-      },
-    ]);
-
-    return popularCourses;
   }
 }
 
